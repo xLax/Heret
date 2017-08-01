a4add92351a72820fda63d420956b23c5ac070ee
xLax <30602376+xLax@users.noreply.github.com>
2017-08-01 12:02:31 +0300

    Fix bugs and added Create User Form

--- HeretPreWorkControl/HeretPreWorkControl/Properties/Resources.Designer.cs
+++ HeretPreWorkControl/HeretPreWorkControl/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace HeretPreWorkControl.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CreateUserTitle {
+            get {
+                object obj = ResourceManager.GetObject("CreateUserTitle", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Decline_Icon {
             get {
                 object obj = ResourceManager.GetObject("Decline_Icon", resourceCulture);
