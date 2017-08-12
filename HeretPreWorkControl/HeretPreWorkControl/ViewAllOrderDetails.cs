a712eb096be206e2e105d758ce94133a97580ced
xLax <30602376+xLax@users.noreply.github.com>
2017-08-12 20:02:20 +0300

    worked on Save button of view all order details screen

--- HeretPreWorkControl/HeretPreWorkControl/ViewAllOrderDetails.cs
+++ HeretPreWorkControl/HeretPreWorkControl/ViewAllOrderDetails.cs
@@ -147,7 +147,189 @@ namespace HeretPreWorkControl
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            Boolean isConvertionOk;
+            int nAmount, nClientID, nFilesNo;
+            string strToConvert;
 
+            this.order.sales_agent_name = tbSalesAgent.Text;
+            this.order.contact_date = dtContactDate.Value.Date;
+            this.order.template_id = tbTemplateNumber.Text;
+            this.order.prisa_id = tbPrisaNumber.Text;
+            this.order.project_desc = tbProjDesc.Text;
+            this.order.order_number = tbOrderNumber.Text;
+            this.order.studio_work = tbStudioWork.Text;
+            this.order.kadas_work = tbKadasWork.Text;
+
+            tbClientNumber.Text = this.order.client_id + "";
+
+            // Convert Files Number
+            strToConvert = tbFilesNo.Text.ToString().Trim(' ');
+
+            if (!strToConvert.Equals(String.Empty))
+            {
+                isConvertionOk = int.TryParse(strToConvert, out nFilesNo);
+
+                if (!isConvertionOk)
+                {
+                    tbFilesNo.BackColor = Color.Tomato;
+                    tbPanel.Text = "שגיאה ! אנא הזן ספרות בלבד בשדה מספר קבצים";
+                }
+                else
+                {
+                    tbFilesNo.BackColor = Color.White;
+                    this.order.files_number = nFilesNo;
+
+                    // Convert amount
+                    strToConvert = tbAmount.Text.ToString().Trim(' ');
+
+                    if (!strToConvert.Equals(String.Empty))
+                    {
+                        isConvertionOk = int.TryParse(strToConvert, out nAmount);
+
+                        if (!isConvertionOk)
+                        {
+                            tbAmount.BackColor = Color.Tomato;
+                            tbPanel.Text = "שגיאה ! אנא הזן ספרות בלבד בשדה כמות";
+                        }
+                        else
+                        {
+                            tbAmount.BackColor = Color.White;
+                            this.order.amount = nAmount;
+
+                            // Convert the client number
+                            strToConvert = tbClientNumber.Text.ToString().Trim(' ');
+
+                            if (!strToConvert.Equals(String.Empty))
+                            {
+                                isConvertionOk = int.TryParse(strToConvert, out nClientID);
+
+                                if (!isConvertionOk)
+                                {
+                                    tbClientNumber.BackColor = Color.Tomato;
+                                    tbPanel.Text = "שגיאה ! אנא הזן ספרות בלבד בשדה מספר לקוח";
+                                }
+                                else
+                                {
+                                    tbClientNumber.BackColor = Color.White;
+                                    this.order.client_id = nClientID;
+                                }
+                            }
+                            else
+                            {
+                                tbPanel.Text = "עליך למלא את השדה מספר לקוח";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        tbPanel.Text = "עליך למלא את השדה כמות";
+                    }
+                }
+            }
+            else
+            {
+                tbPanel.Text = "עליך למלא את השדה מספר קבצים";
+            }
         }
+        //if (lbPriseTempDesc.SelectedItem == null)
+        //{
+        //    tbPanel.Text = "שגיאה ! יש לבחור באחת האופציות מהרשימה";
+        //}
+        //else if (tbPrisaTempDesc.Text.ToString().Trim(' ').Equals(String.Empty))
+        //{
+        //    tbPrisaTempDesc.BackColor = Color.Tomato;
+        //    tbPanel.Text = "שגיאה ! השדה " + lbPriseTempDesc.SelectedItem.ToString() + " ריק!";
+        //}
+        //// נשאר רק השדרה כמות
+        //else
+        //{
+        //    tbPrisaTempDesc.BackColor = Color.White;
+        //    strToConvert = tbAmount.Text.ToString().Trim(' ');
+        //    isConvertionOk = int.TryParse(strToConvert, out nAmount);
+
+        //    if (!isConvertionOk)
+        //    {
+        //        tbAmount.BackColor = Color.Tomato;
+        //        tbPanel.Text = "שגיאה ! אנא הזן ספרות בשדה כמות";
+        //    }
+        //    else
+        //    {
+        //        tbAmount.BackColor = Color.White;
+
+        //        // הכל טוב
+        //        NewOrder.ID = Utilities.GetNextOrderID();
+
+        //        if (NewOrder.ID != -1)
+        //        {
+        //            NewOrder.client_id = int.Parse(tbClientNumber.Text.ToString());
+        //            NewOrder.sales_agent_name = Globals.Name;
+
+        //            NewOrder.dep_recieve_date = DateTime.Today.Date;
+        //            NewOrder.dep_recieve_hour = DateTime.Now.TimeOfDay;
+
+        //            NewOrder.contact_date = dtContactDate.Value;
+        //            NewOrder.creation_date = DateTime.Today.Date;
+
+        //            NewOrder.action_type_id = Globals.ActionTypeNewOrder;
+        //            NewOrder.amount = nAmount;
+        //            NewOrder.files_number = nFilesNo;
+
+        //            NewOrder.current_status_id = Globals.StatusInWork;
+
+        //            if (lbPriseTempDesc.SelectedItem.ToString().Equals(Globals.TemplateNumber))
+        //            {
+        //                NewOrder.template_id = tbPrisaTempDesc.Text.ToString();
+        //            }
+        //            else if (lbPriseTempDesc.SelectedItem.ToString().Equals(Globals.PrisaNumber))
+        //            {
+        //                NewOrder.prisa_id = tbPrisaTempDesc.Text.ToString();
+        //            }
+        //            else if (lbPriseTempDesc.SelectedItem.ToString().Equals(Globals.ProjectDesc))
+        //            {
+        //                NewOrder.project_desc = tbPrisaTempDesc.Text.ToString();
+        //            }
+
+        //            string strPromoteMessage = String.Empty;
+
+        //            if (cbMoveToManager.Checked)
+        //            {
+        //                // Shit you mother fucker
+        //                NewOrder.special_department_id = Globals.AdminID;
+        //                strPromoteMessage = "ובקשת קידום נשלחה";
+        //            }
+        //            else
+        //            {
+        //                // Keep calm
+        //            }
+
+        //            if (NewOrder.creation_date.Value.AddDays(-2) >= NewOrder.contact_date)
+        //            {
+        //                NewOrder.alert_creation_date = Globals.AlertNow;
+        //            }
+
+        //            using (var context = new DB_Entities())
+        //            {
+        //                try
+        //                {
+        //                    context.tbl_orders.Add(NewOrder);
+        //                    context.SaveChanges();
+
+        //                    tbPanel.Text = "ההזמנה נוצרה בהצלחה ! " + strPromoteMessage;
+
+        //                    isSucceeded = true;
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    tbPanel.Text = "שגיאה! החיבור לבסיס הנתונים כשל";
+        //                }
+        //            }
+        //        }
+        //        else
+        //        {
+        //            tbPanel.Text = "שגיאה! החיבור לבסיס הנתונים כשל";
+        //        }
+        //    }
+        //}
+
     }
 }
