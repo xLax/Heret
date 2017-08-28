c7e04686da9d0bef60b8eab786518161688e5b79
xLax <30602376+xLax@users.noreply.github.com>
2017-08-28 17:12:23 +0300

    גיטלר

---
+++ HeretPreWorkControl/HeretPreWorkControl/InsertTemplateNumberForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace HeretPreWorkControl
+{
+    partial class InsertTemplateNumberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InsertTemplateNumberForm));
+            this.tbPanel = new System.Windows.Forms.TextBox();
+            this.tbModelNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbExecute = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExecute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbPanel
+            // 
+            this.tbPanel.BackColor = System.Drawing.SystemColors.Info;
+            this.tbPanel.Enabled = false;
+            this.tbPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.tbPanel.Location = new System.Drawing.Point(13, 194);
+            this.tbPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPanel.Multiline = true;
+            this.tbPanel.Name = "tbPanel";
+            this.tbPanel.Size = new System.Drawing.Size(445, 35);
+            this.tbPanel.TabIndex = 34;
+            this.tbPanel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbModelNum
+            // 
+            this.tbModelNum.BackColor = System.Drawing.SystemColors.Window;
+            this.tbModelNum.Font = new System.Drawing.Font("David", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.tbModelNum.Location = new System.Drawing.Point(144, 98);
+            this.tbModelNum.Margin = new System.Windows.Forms.Padding(4);
+            this.tbModelNum.Name = "tbModelNum";
+            this.tbModelNum.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tbModelNum.Size = new System.Drawing.Size(313, 37);
+            this.tbModelNum.TabIndex = 33;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("David", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 27);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "מספר דגם:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::HeretPreWorkControl.Properties.Resources.EnterModelTitle;
+            this.pictureBox2.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(207, 62);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 31;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("David", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(253, 155);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 27);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "הזן מספר דגם";
+            // 
+            // pbExecute
+            // 
+            this.pbExecute.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbExecute.Image = global::HeretPreWorkControl.Properties.Resources.Go_icon;
+            this.pbExecute.Location = new System.Drawing.Point(413, 148);
+            this.pbExecute.Margin = new System.Windows.Forms.Padding(4);
+            this.pbExecute.Name = "pbExecute";
+            this.pbExecute.Size = new System.Drawing.Size(47, 38);
+            this.pbExecute.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbExecute.TabIndex = 29;
+            this.pbExecute.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HeretPreWorkControl.Properties.Resources.Heret_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(256, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 28;
+            this.pictureBox1.TabStop = false;
+            // 
+            // InsertTemplateNumberForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(472, 240);
+            this.Controls.Add(this.tbPanel);
+            this.Controls.Add(this.tbModelNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbExecute);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "InsertTemplateNumberForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "מערכת לבקרת תהליך קדם העבודה";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExecute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPanel;
+        private System.Windows.Forms.TextBox tbModelNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pbExecute;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
